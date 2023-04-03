634c5a4e7061be6f926813af6a0caba664b4826f
Irfan Maulana <irfanzulkifli@gmail.com>
2023-04-03 16:33:13 +0700

    Changed price ot update every 10am

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ async function main() {
     logger.info(`Main function started`)
 
     /** RUN PRICE COLLECTION EVERYDAY AT 1AM **/
-    const job = schedule.scheduleJob('0 1 * * *', async function(fireDate){
+    const job = schedule.scheduleJob('0 10 * * *', async function(fireDate){
         logger.info(` --- SYSTEM PRICE COLLECTION STARTED ---`);
         logger.info('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
         let collectedPrice = await CollectPrice(db, "", "SYSTEM")
