0fd13ce5f74b5f318aeaffa7f425b7ed143363da
Irfan Maulana <irfanzulkifli@gmail.com>
2023-04-03 16:20:42 +0700

    Added price to run every 1am

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 const schedule = require('node-schedule');
 
-
 import CollectCards from "./collect/collectCards";
 import CollectSets from "./collect/collectSets";
 import CollectPrice from "./collect/collectPrice";
-
-
 
 import DBConfig from './utils/db_config';
 const pgp = require('pg-promise')();
@@ -25,47 +22,23 @@ const logger = createLogger({
       ),
     transports: [
       new transports.File({ filename: 'logs/main.log'}),
+      new transports.Console()
     ],
 });
 
 
-async function main1() {
+async function main() {
     logger.info(`Main function started`)
 
-
-    //Date
-    const timeElapsed = Date.now();
-    const today = new Date(timeElapsed);
-
-    /** SET COLLECTION FLAGS*/
-    let SetMetaFlag = true;
-    let SetImagesFlag = false;
-
-    // //Every Saturday (Day 6) Or First of Month, Collect Images
-    // if (today.getDay() == 6 || today.getDate() == 1 ) SetImagesFlag = true;
-    // console.log(`${today.toUTCString()}`," - ", ` STARTING SET COLLECTION: (${SetMetaFlag} ${SetImagesFlag})`)
-    let collectedSets = await CollectSets(db, SetMetaFlag, SetImagesFlag, "SYSTEM");
-    // console.log(`${today.toUTCString()}`," - ", ` COMPLETED SET COLLECTION: Result: ${collectedSets}`)
-
-    // /** CARD COLLECTION FLAGS*/
-    // let CardMetaFlag = true;
-    // let CardImageFlag = false;
-
-    // //Every Two Weeks, Collect Images
-    // if (today.getDate() == 1 || today.getDate() == 14) CardImageFlag = true;
-    // console.log(`${today.toUTCString()}`," - ", ` STARTING CARD COLLECTION: (${SetMetaFlag} ${CardImageFlag})`)
-    // let collectedCards = await CollectCards(db, CardMetaFlag, CardImageFlag);
-    // console.log(`${today.toUTCString()}`," - ", ` COMPLETED CARD COLLECTION: Result: ${collectedCards}`)
+    /** RUN PRICE COLLECTION EVERYDAY AT 1AM **/
+    const job = schedule.scheduleJob('0 1 * * *', async function(fireDate){
+        logger.info(` --- SYSTEM PRICE COLLECTION STARTED ---`);
+        logger.info('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+        let collectedPrice = await CollectPrice(db, "", "SYSTEM")
+        logger.info(collectedPrice);
+        logger.info(` --- SYSTEM PRICE COLLECTION COMPLETED ---`);
+      });
 
 }
-
-async function main() {
-    // const job = schedule.scheduleJob('* * * * *', function(fireDate){
-    //     console.log('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
-    //   });
-
-    await CollectPrice(db, "", "SYSTEM");
-}
-
 main();
 
