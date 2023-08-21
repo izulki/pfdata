2939d3a1c7726984eb7f241cf07a6ee5458879fa
Irfan <irfanzulkifli@gmail.com>
2023-08-21 14:31:32 -0400

    Added analysis to list of jobs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ const schedule = require('node-schedule');
 import CollectCards from "./collect/collectCards";
 import CollectSets from "./collect/collectSets";
 import CollectPrice from "./collect/collectPrice";
+import CollectAnalysis from "./collect/collectAnalysis";
+
 
 import DBConfig from './utils/db_config';
 const pgp = require('pg-promise')();
@@ -30,13 +32,19 @@ const logger = createLogger({
 async function main() {
     logger.info(`Main function started`)
 
-    /** RUN PRICE COLLECTION EVERYDAY AT 1AM **/
+    /** RUN COLLECTION EVERYDAY AT 1AM **/
     const job = schedule.scheduleJob('0 10 * * *', async function(fireDate){
         logger.info(` --- SYSTEM PRICE COLLECTION STARTED ---`);
         logger.info('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
         let collectedPrice = await CollectPrice(db, "", "SYSTEM")
         logger.info(collectedPrice);
         logger.info(` --- SYSTEM PRICE COLLECTION COMPLETED ---`);
+
+        logger.info(` --- SYSTEM ANALYSIS COLLECTION STARTED ---`);
+        logger.info('This job was supposed to run at ' + fireDate + ', but actually ran at ' + new Date());
+        let collectedAnalysis = await CollectAnalysis(db, "SYSTEM")
+        logger.info(collectedAnalysis);
+        logger.info(` --- SYSTEM ANALYSIS COLLECTION COMPLETED ---`);
       });
 
 }
